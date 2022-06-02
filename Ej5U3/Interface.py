2c4c1730023b175a0854103f01b919841588bee3
Alejo Atienza <73915312+Alekitas@users.noreply.github.com>
2022-06-01 21:22:20 -0300

    Add files via upload

---
+++ Ej5U3/Interface.py
@@ -0,0 +1,9 @@
+from zope.interface import interface
+from zope.interface import implementer
+class Interface(interface):
+    def instertarElemento(elemento,pos):
+        pass
+    def AgregarElemento(elemento):
+        pass
+    def mostrarElemento(pos):
+        pass
